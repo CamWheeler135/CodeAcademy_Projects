63c588c862a4f47ab110d851c02edb70f0efb0b4
Cameron Wheeler <98394754+CamWheeler135@users.noreply.github.com>
2023-05-23 18:26:01 +0100

    Addition of custom errors

---
+++ ML_Engineer_Career_Path/Main_Disease_Classifier_with_CNN/src/errors.py
@@ -0,0 +1,14 @@
+''' File that contains the custom errors. '''
+
+class FileNotFoundError(Exception):
+    ''' This error exists to raise errors if the user tries to collect data from an empty directory.'''
+
+    def __str__(self) -> str:
+        return f"No files found in the directory, ensure that the path name is correct or that files are loaded in the correct directory."
+    
+
+class DiseaseNotSupportedError(Exception):
+    ''' This error exists to raise errors if users try to feed cancer type data into the pipeline that the model has not been trained on. '''
+
+    def __str__(self, cancer_type) -> str:
+        return f"{cancer_type} cancer is not in the supported list, of cancer types. Check filename or file before continuing."
