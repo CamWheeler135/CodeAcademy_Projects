e24d3d839dafbe600883dbe987454cbc8bc22fc2
Cameron Wheeler <98394754+CamWheeler135@users.noreply.github.com>
2023-06-13 12:49:02 +0100

    Updated main code for PCA analysis and complete plotting

--- ML_Engineer_Career_Path/Main_Disease_Classifier_with_CNN/main.py
+++ ML_Engineer_Career_Path/Main_Disease_Classifier_with_CNN/main.py
@@ -3,21 +3,43 @@ from pathlib import Path
 import time
 
 # SRC Imports.
-from src.data_cleaning import DataCleaner
-from src.eda import ExploratoryAnalyzer, Plotter
+from src.data_cleaning import DataCleaner, DataHandler
+from src.eda import ExploratoryAnalyzer, UnsupervisedVisualizer, Plotter
+from src.preprocessing import UnsupervisedPreprocessor
 
 def main():
-    # cleaner = DataCleaner()
-    # cleaner.complete_clean()
-    eda = ExploratoryAnalyzer()
-    repertoires = eda.collect_files()
-    eda.count_sequences_per_disease(repertoires)
-    plotter = Plotter(eda)
-    plotter.plot_sequence_count_pie()
+    ## Data Cleaning. 
+    dc_dh = DataHandler(collection_path=Path('data/raw_files/'), save_path=Path('data/cleaned_files/'))
+    data_cleaner = DataCleaner(dc_dh)
+    data_cleaner.complete_clean()
+
+    ## Exploratory Data Analysis.
+    eda_dh = DataHandler(collection_path=Path('data/cleaned_files'))
+    eda = ExploratoryAnalyzer(eda_dh)
+    eda.complete_eda()
+
+
+    ## Unsupervised Preprocessing.
+    usp_dh = DataHandler(
+        collection_path=Path('data/cleaned_files'), 
+        save_path=Path('data/processed_files/kmers/kmers.csv'))
+    usp = UnsupervisedPreprocessor(usp_dh)
+    usp.complete_preprocess()
+
+    ## PCA Analysis
+    pca_dh = DataHandler(collection_path=Path('data/processed_files/kmers'),
+                         save_path=Path('data/processed_files/pca/pca_df.csv'))
+    pca = UnsupervisedVisualizer(pca_dh)
+    pca.complete_pca_analysis()
+
+    ## Plotting
+    plotter = Plotter(eda, pca)
+    plotter.complete_plots()
+
+
 
 if __name__ == "__main__":
     main()
-
 
 
 
